7ebec8a14ecdcb7bb5e581f82fc1167f63d9b76c
Scarlet <lettie@catius.io>
2022-12-21 14:20:13 +0900

    데이터베이스를 singleton으로 유지해야하는 이유

--- src/main/java/CommandRouterFactory.java
+++ src/main/java/CommandRouterFactory.java
@@ -1,5 +1,8 @@
 import dagger.Component;
 
+import javax.inject.Singleton;
+
+@Singleton
 @Component(modules = {HelloWorldModule.class, LoginCommandModule.class, UserCommandsModule.class, SystemOutModule.class})
 interface CommandRouterFactory {
     CommandRouter router();
