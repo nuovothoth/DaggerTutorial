9d00453f9ff31f041f0fdd18bfb32c36f8fcce06
Scarlet <lettie@catius.io>
2022-12-21 16:01:26 +0900

    CommandProcessor을 도입했지만 아무것도 처리되지 않는 상태

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,12 +1,28 @@
 import java.util.List;
+import java.util.Optional;
 
 public interface Command {
     String key();
 
     Status handleInput(List<String> input);
 
+    class Result {
+        private final Status status;
+        private final Optional<CommandRouter> nestedCommandRouter;
+
+        Result(Status status, Optional<CommandRouter> nestedCommandRouter) {
+            this.status = status;
+            this.nestedCommandRouter = nestedCommandRouter;
+        }
+
+        static Result enterNestedCommandSet(CommandRouter nestedCommandRouter) {
+            return new Result(Status.HANDLED, Optional.of(nestedCommandRouter));
+        }
+    }
+
     enum Status {
         INVALID,
-        HANDLED
+        HANDLED,
+        INPUT_COMPLETED
     }
 }
--- src/main/java/CommandLineAtm.java
+++ src/main/java/CommandLineAtm.java
@@ -1,14 +1,22 @@
+import dagger.Component;
+
+import javax.inject.Singleton;
 import java.util.Scanner;
 
 public class CommandLineAtm {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        CommandRouter commandRouter = new CommandRouter();
-        CommandRouterFactory commandRouterFactory = DaggerCommandRouterFactory.create();
-        CommandRouter commandRouter = commandRouterFactory.router();
+        CommandProcessor commandProcessor =
+                DaggerCommandLineAtm_CommandProcessorFactory.create().processor();
 
         while (scanner.hasNextLine()) {
-            commandRouter.route(scanner.nextLine());
+            commandProcessor.process(scanner.nextLine());
         }
+    }
+
+    @Singleton
+    @Component(modules = {HelloWorldModule.class, LoginCommandModule.class, UserCommandsModule.class, SystemOutModule.class})
+    interface CommandProcessorFactory {
+        CommandProcessor processor();
     }
 }
