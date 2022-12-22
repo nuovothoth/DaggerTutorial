27292565e1537ece2ef1226fb4b2e8ea00b2f5bd
Scarlet <lettie@catius.io>
2022-12-22 13:16:15 +0900

    로그아웃 없이 로그인 되는 문제 수정

--- src/main/java/LoginCommand.java
+++ src/main/java/LoginCommand.java
@@ -1,24 +1,34 @@
 import javax.inject.Inject;
+import java.util.Optional;
 
 final class LoginCommand extends SingleArgCommand {
     private final Outputter outputter;
     private final Database database;
     private final UserCommandsRouter.Factory userCommandsRouterFactory;
+    private final Optional<Database.Account> account;
 
     @Inject
     LoginCommand(
             Database database,
             Outputter outputter,
-            UserCommandsRouter.Factory userCommandsRouterFactory
+            UserCommandsRouter.Factory userCommandsRouterFactory,
+            Optional<Database.Account> account
     ) {
         System.out.println("Creating a new " + this);
         this.database = database;
         this.outputter = outputter;
         this.userCommandsRouterFactory = userCommandsRouterFactory;
+        this.account = account;
     }
 
     @Override
     public Result handleArg(String username) {
+        if (account.isPresent()) {
+            // Ignore "login <foo>" commands if we already have an account
+            outputter.output("Please logout of " + account.get().username() + " before login of " + username);
+            return Result.handled();
+        }
+
         Database.Account account = database.getAccount(username);
         outputter.output(username + " is logged in with balance: " + account.balance());
         return Result.enterNestedCommandSet(
--- src/main/java/LoginCommandModule.java
+++ src/main/java/LoginCommandModule.java
@@ -1,17 +1,21 @@
 import dagger.Binds;
+import dagger.BindsOptionalOf;
 import dagger.Module;
 import dagger.multibindings.IntoMap;
 import dagger.multibindings.StringKey;
 
 @Module
-abstract class LoginCommandModule {
+interface LoginCommandModule {
     @Binds
     @IntoMap
     @StringKey("login")
-    abstract Command loginCommand(LoginCommand command);
+    Command loginCommand(LoginCommand command);
 
     @Binds
     @IntoMap
     @StringKey("logout")
-    abstract Command logoutCommand(LogoutCommand command);
+    Command logoutCommand(LogoutCommand command);
+
+    @BindsOptionalOf
+    Database.Account optionalAccount();
 }
